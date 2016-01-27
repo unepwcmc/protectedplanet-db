787082dcf5a5058fcf923e50ab585726b70cbc53
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-01-27 20:29:44 +0000

    add pame_statistics table

---
+++ migrate/20160127195755_create_pame_statistics_table.rb
@@ -0,0 +1,12 @@
+class CreatePameStatisticsTable < ActiveRecord::Migration
+  def change
+    create_table :pame_statistics_tables do |t|
+      t.belongs_to :country
+      t.integer    :assessments
+      t.integer    :assessed_pas
+      t.float      :average_score
+      t.float      :total_area_assessed
+      t.float      :percentage_area_assessed
+    end
+  end
+end
