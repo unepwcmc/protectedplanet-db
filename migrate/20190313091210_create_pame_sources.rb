9d9c6ea5dd1190a2035006d23c6df8b0937e37ec
Andrew Potter <hello@drewpotter.com>
2019-03-13 14:10:54 +0000

    Add pame sources and source reference to pame evaluations

---
+++ migrate/20190313091210_create_pame_sources.rb
@@ -0,0 +1,12 @@
+class CreatePameSources < ActiveRecord::Migration
+  def change
+    create_table :pame_sources do |t|
+      t.string :data_title
+      t.string :resp_party
+      t.integer :year
+      t.string :language
+
+      t.timestamps
+    end
+  end
+end
