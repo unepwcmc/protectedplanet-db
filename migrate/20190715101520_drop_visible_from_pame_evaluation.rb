3513eb2226f06feaf7f84ffecf7cbfaf738d0652
Andrew Potter <hello@drewpotter.com>
2019-07-15 15:42:28 +0100

    Fix migration so it works on rails 4

--- migrate/20190715101520_drop_visible_from_pame_evaluation.rb
+++ migrate/20190715101520_drop_visible_from_pame_evaluation.rb
@@ -1,4 +1,4 @@
-class DropVisibleFromPameEvaluation < ActiveRecord::Migration[5.0]
+class DropVisibleFromPameEvaluation < ActiveRecord::Migration
   def change
     remove_column :pame_evaluations, :visible
   end
