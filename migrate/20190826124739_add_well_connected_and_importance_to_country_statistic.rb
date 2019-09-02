300a232940f11860001407e07c48418788091c63
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-09-02 11:58:37 +0100

    Add stats for marine as well

--- migrate/20190826124739_add_well_connected_and_importance_to_country_statistic.rb
+++ migrate/20190826124739_add_well_connected_and_importance_to_country_statistic.rb
@@ -1,6 +1,8 @@
 class AddWellConnectedAndImportanceToCountryStatistic < ActiveRecord::Migration
   def change
-    add_column :country_statistics, :well_connected, :float
-    add_column :country_statistics, :importance, :float
+    add_column :country_statistics, :percentage_land_well_connected, :float
+    add_column :country_statistics, :percentage_marine_well_connected, :float
+    add_column :country_statistics, :percentage_land_importance, :float
+    add_column :country_statistics, :percentage_marine_importance, :float
   end
 end
