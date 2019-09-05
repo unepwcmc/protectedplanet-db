87a5c7f1300db6a65fb790abf06f609f5d719627
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-09-05 09:45:37 +0100

    Rename global percentages columns and remove land and marine

--- migrate/20190826124739_add_well_connected_and_importance_to_country_statistic.rb
+++ migrate/20190826124739_add_well_connected_and_importance_to_country_statistic.rb
@@ -1,8 +1,6 @@
 class AddWellConnectedAndImportanceToCountryStatistic < ActiveRecord::Migration
   def change
-    add_column :country_statistics, :percentage_land_well_connected, :float
-    add_column :country_statistics, :percentage_marine_well_connected, :float
-    add_column :country_statistics, :percentage_land_importance, :float
-    add_column :country_statistics, :percentage_marine_importance, :float
+    add_column :country_statistics, :percentage_well_connected, :float
+    add_column :country_statistics, :percentage_importance, :float
   end
 end
