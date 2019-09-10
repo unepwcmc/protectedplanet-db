02b266bd499bf66c2f0db0e4a80c360980a2d64c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-09-10 12:12:44 +0100

    Update migration to include representative stat

--- migrate/20190910102906_make_target_representative_well_connected_and_importance_global_stats.rb
+++ migrate/20190910102906_make_target_representative_well_connected_and_importance_global_stats.rb
@@ -1,5 +1,8 @@
-class MakeTargetWellConnectedAndImportanceGlobalStats < ActiveRecord::Migration[5.0]
+class MakeTargetRepresentativeWellConnectedAndImportanceGlobalStats < ActiveRecord::Migration[5.0]
   def change
+    rename_column :aichi11_targets, :representative_terrestrial, :representative_global
+    remove_column :aichi11_targets, :representative_marine, :float
+
     rename_column :aichi11_targets, :well_connected_terrestrial, :well_connected_global
     remove_column :aichi11_targets, :well_connected_marine, :float
 
