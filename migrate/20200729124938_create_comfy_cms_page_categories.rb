4b4dc2742c23a8267abc9570194d116da52a6e53
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-07-30 11:36:51 +0100

    Add tables to link layouts to categories groups

--- migrate/20200729124938_create_comfy_cms_page_categories.rb
+++ migrate/20200729124938_create_comfy_cms_page_categories.rb
@@ -1,7 +1,20 @@
 class CreateComfyCmsPageCategories < ActiveRecord::Migration[5.2]
   def change
+    create_table :comfy_cms_layout_categories do |t|
+      t.string :label
+
+      t.timestamps
+    end
+
+    create_table :comfy_cms_layouts_categories do |t|
+      t.references :layout, foreign_key: { to_table: :comfy_cms_layouts }
+      t.references :layout_category, foreing_key: { to_table: :comfy_cms_layout_categories }
+
+      t.timestamps
+    end
+
     create_table :comfy_cms_page_categories do |t|
-      t.string :group_name
+      t.references :layout_category, foreign_key: { to_table: :comfy_cms_layout_categories }
       t.string :label
 
       t.timestamps
