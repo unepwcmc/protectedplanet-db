183dd16e8b18a5344050e9bc5842c037a0c5969d
Stanley Liu <sliu1993@hotmail.co.uk>
2020-09-15 16:18:31 +0100

    corrected column type to date instead of year

--- migrate/20200915143143_add_column_update_year_to_source_table.rb
+++ migrate/20200915143143_add_column_update_year_to_source_table.rb
@@ -1,5 +1,5 @@
 class AddColumnUpdateYearToSourceTable < ActiveRecord::Migration[5.2]
   def change
-    add_column :sources, :update_year, :year
+    add_column :sources, :update_year, :date
   end
 end
