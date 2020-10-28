748d0561d356cbf855b6ad75303446736b25a93f
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-28 12:22:12 +0000

    add comment to migration file to explain what the url represents

--- migrate/20201027164524_add_gl_url_to_protected_areas.rb
+++ migrate/20201027164524_add_gl_url_to_protected_areas.rb
@@ -1,5 +1,6 @@
 class AddGlUrlToProtectedAreas < ActiveRecord::Migration[5.2]
   def change
+    # Link for GL sites only that goes to the equivalent site on the IUCN website
     add_column :protected_areas, :green_list_url, :string
   end
 end
