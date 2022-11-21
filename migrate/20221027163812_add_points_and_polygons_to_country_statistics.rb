e43c3970e4be7a26a9bdf2f6236adf916080aa59
sergio <s.marrocoli@gmail.com>
2022-11-21 16:45:55 +0000

    feat: reduce activerecord::migration version

--- migrate/20221027163812_add_points_and_polygons_to_country_statistics.rb
+++ migrate/20221027163812_add_points_and_polygons_to_country_statistics.rb
@@ -1,4 +1,4 @@
-class AddPointsAndPolygonsToCountryStatistics < ActiveRecord::Migration[5.2]
+class AddPointsAndPolygonsToCountryStatistics < ActiveRecord::Migration[5.0]
   def change
     add_column :country_statistics, :oecm_polygons, :integer
     add_column :country_statistics, :oecm_points, :integer
