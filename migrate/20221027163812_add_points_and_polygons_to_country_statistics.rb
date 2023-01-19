71c02aa87d6c93163c77989ca977b4d058858689
maxime <maxycle@hotmail.com>
2023-01-19 16:18:44 +0000

    Revert "Merge pull request #50 from unepwcmc/feat/add-point/polygons-count-to-country-statistic-for-OECM/PAs"
    This reverts commit 415de70342b09946245ca3595d88cbb0f9575746, reversing
    changes made to f7477444d2d95625fc84c7feac40b59e4b282b93.

--- migrate/20221027163812_add_points_and_polygons_to_country_statistics.rb
+++
@@ -1,8 +0,0 @@
-class AddPointsAndPolygonsToCountryStatistics < ActiveRecord::Migration[5.2]
-  def change
-    add_column :country_statistics, :oecm_polygons, :integer
-    add_column :country_statistics, :oecm_points, :integer
-    add_column :country_statistics, :protected_area_polygons, :integer
-    add_column :country_statistics, :protected_area_points, :integer
-  end
-end
