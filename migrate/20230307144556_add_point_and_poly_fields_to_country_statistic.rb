55bcf50bd49871880b09ae72e22f15b0c033b6d3
sergiomarrocoli <s.marrocoli@gmail.com>
2023-03-07 15:25:58 +0000

    feat: update new column names in migration to indicate that they are counts

--- migrate/20230307144556_add_point_and_poly_fields_to_country_statistic.rb
+++ migrate/20230307144556_add_point_and_poly_fields_to_country_statistic.rb
@@ -1,8 +1,8 @@
 class AddPointAndPolyFieldsToCountryStatistic < ActiveRecord::Migration[5.2]
   def change
-    add_column :country_statistics, :oecm_polygons, :integer
-    add_column :country_statistics, :oecm_points, :integer
-    add_column :country_statistics, :protected_area_polygons, :integer
-    add_column :country_statistics, :protected_area_points, :integer
+    add_column :country_statistics, :oecm_polygon_count, :integer
+    add_column :country_statistics, :oecm_point_count, :integer
+    add_column :country_statistics, :protected_area_polygon_count, :integer
+    add_column :country_statistics, :protected_area_point_count, :integer
   end
 end
