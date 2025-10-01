b6dfea9bedaf3e8e0adb6a705bbb469067fdf42b
Yueeeeee <yue-long.he@unep-wcmc.org>
2025-10-01 08:50:20 +0100

    Update 20251001081500_add_green_list_status_to_protected_area_parcels.rb

--- migrate/20251001081500_add_green_list_status_to_protected_area_parcels.rb
+++ migrate/20251001081500_add_green_list_status_to_protected_area_parcels.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 class AddGreenListStatusToProtectedAreaParcels < ActiveRecord::Migration[5.2]
-  def up
-    add_reference :protected_area_parcels, :green_list_status,
-      foreign_key: { to_table: :green_list_statuses }
-    add_index :protected_area_parcels, :green_list_status_id
-  end
-
-  def down
-    remove_index :protected_area_parcels, :green_list_status_id
-    remove_reference :protected_area_parcels, :green_list_status
+  def change
+    add_reference :protected_area_parcels, :green_list_status, foreign_key: true
   end
 end
